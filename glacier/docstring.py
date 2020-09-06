8790c002a34b292771c05f74808899f552250790
Hiroki Konishi <hkonishi@yahoo-corp.jp>
2020-09-06 15:36:48 +0900

    feat: docstring parser for numpy and reStructuredText

--- glacier/docstring.py
+++ glacier/docstring.py
@@ -2,14 +2,18 @@
 Parser of docstring
 """
 from __future__ import annotations
+from pprint import pformat
 
 import re
+from enum import Enum, auto
 from typing import List
 from dataclasses import dataclass
 
 from typing_extensions import Protocol
 
-ARG_START_PATTERN = re.compile(r'^(\w+): ')
+GOOGLE_ARG_START_PATTERN = re.compile(r'^(\w+): ')
+NUMPY_ARG_START_PATTERN = re.compile(r'^(\w+):')
+RESTTXT_ARG_START_PATTERN = re.compile(r'^:param (\w+):')
 
 
 @dataclass(init=False)
@@ -50,7 +54,7 @@ class Arg:
     description: str
 
     @classmethod
-    def of_lines(cls, lines: List[str]) -> List[Arg]:
+    def of_lines_google(cls, lines: List[str]) -> List[Arg]:
         """
         Parse the lines of args as following and return the list.
 
@@ -62,7 +66,7 @@ class Arg:
         arg_name = ''
         arg_description_builder = DescriptionBuilder()
         for line in lines:
-            m = re.search(ARG_START_PATTERN, line.lstrip())
+            m = re.search(GOOGLE_ARG_START_PATTERN, line.lstrip())
             if m:
                 if arg_name:
                     args.append(Arg(
@@ -72,7 +76,77 @@ class Arg:
                     arg_description_builder = DescriptionBuilder()
                 arg_name = m.group(1)
                 arg_description_builder.add_line(re.sub(
-                    ARG_START_PATTERN,
+                    GOOGLE_ARG_START_PATTERN,
+                    '',
+                    line.lstrip(),
+                ).strip())
+            else:
+                arg_description_builder.add_line(line.strip())
+
+        if arg_name:
+            args.append(Arg(
+                name=arg_name,
+                description=arg_description_builder.build(),
+            ))
+        return args
+
+    @classmethod
+    def of_lines_numpy(cls, lines: List[str]) -> List[Arg]:
+        """
+        Parse the lines of numpy args format as following and return the list.
+
+        hoge: str
+            description of hoge
+        fuga:
+            description of fuga.
+        """
+        args = []
+        arg_name = ''
+        arg_description_builder = DescriptionBuilder()
+        for line in lines:
+            m = re.search(NUMPY_ARG_START_PATTERN, line.lstrip())
+            if m:
+                if arg_name:
+                    args.append(Arg(
+                        name=arg_name,
+                        description=arg_description_builder.build(),
+                    ))
+                    arg_description_builder = DescriptionBuilder()
+                arg_name = m.group(1)
+            else:
+                arg_description_builder.add_line(line.strip())
+
+        if arg_name:
+            args.append(Arg(
+                name=arg_name,
+                description=arg_description_builder.build(),
+            ))
+        return args
+
+    @classmethod
+    def of_lines_resttxt(cls, lines: List[str]) -> List[Arg]:
+        """
+        Parse the lines of reStructuredText args format as following
+        and return the list.
+
+        :param hoge: description of hoge
+        :param fuga: description of fuga
+        """
+        args = []
+        arg_name = ''
+        arg_description_builder = DescriptionBuilder()
+        for line in lines:
+            m = re.search(RESTTXT_ARG_START_PATTERN, line.lstrip())
+            if m:
+                if arg_name:
+                    args.append(Arg(
+                        name=arg_name,
+                        description=arg_description_builder.build(),
+                    ))
+                    arg_description_builder = DescriptionBuilder()
+                arg_name = m.group(1)
+                arg_description_builder.add_line(re.sub(
+                    RESTTXT_ARG_START_PATTERN,
                     '',
                     line.lstrip(),
                 ).strip())
@@ -116,9 +190,59 @@ class GoogleParser:
                 ends_args_section = True
             elif not ends_args_section:
                 args_lines.append(line)
-        return Doc(description_builder.build(), Arg.of_lines(args_lines))
+        return Doc(description_builder.build(), Arg.of_lines_google(args_lines))
+
+
+class NumpyParserState(Enum):
+    IN_DESCRIPTION = auto()
+    FOUND_PARAMETERS = auto()
+    FOUND_PARAMETERS_LINE = auto()
 
 
 class NumpyParser:
+
     def parse(self, docstring: str) -> Doc:
-        return Doc('', [])
+        docstring_lines = docstring.splitlines()
+        description_builder = DescriptionBuilder()
+        state: NumpyParserState = NumpyParserState.IN_DESCRIPTION
+        args_lines = []
+        for i, line in enumerate(docstring_lines):
+            if state == NumpyParserState.IN_DESCRIPTION:
+                if line.strip() == 'Parameters':
+                    state = NumpyParserState.FOUND_PARAMETERS
+                    continue
+                description_builder.add_line(line)
+            elif state == NumpyParserState.FOUND_PARAMETERS:
+                if line.strip() == '----------':
+                    state = NumpyParserState.FOUND_PARAMETERS_LINE
+                    continue
+            elif state == NumpyParserState.FOUND_PARAMETERS_LINE:
+                args_lines.append(line)
+        return Doc(description_builder.build(), Arg.of_lines_numpy(args_lines))
+
+
+RESTTXT_ITEM_PATTERN = re.compile(r'^:(\w+) (\w+):')
+
+
+class RestructuredTextParser:
+
+    def parse(self, docstring: str) -> Doc:
+        docstring_lines = docstring.splitlines()
+        description_builder = DescriptionBuilder()
+        started_args = False
+        args_lines = []
+        for i, line in enumerate(docstring_lines):
+            m = re.search(RESTTXT_ITEM_PATTERN, line.lstrip())
+            if m:
+                item_name = m.group(1)
+                if item_name == 'param':
+                    started_args = True
+                    args_lines.append(line)
+                else:
+                    break
+            elif started_args:
+                args_lines.append(line)
+            else:
+                description_builder.add_line(line)
+
+        return Doc(description_builder.build(), Arg.of_lines_resttxt(args_lines))
--- tests/test_docstring.py
+++ tests/test_docstring.py
@@ -1,28 +1,52 @@
 import unittest
 
-from glacier.docstring import Arg, Doc, GoogleParser
+from glacier.docstring import Arg, Doc, GoogleParser, NumpyParser
+
+from glacier.docstring import RestructuredTextParser
 
 
 class TestArg(unittest.TestCase):
-    def test_arg_of_lines(self) -> None:
+    def test_arg_of_lines_google(self) -> None:
         arg_lines = [
             'hoge: description of hoge',
             '      that can be multilined.',
             'fuga: description of fu-',
             '      ga.',
         ]
-        args = Arg.of_lines(arg_lines)
+        args = Arg.of_lines_google(arg_lines)
         assert args == [
             Arg(name='hoge', description='description of hoge that can be multilined.'),
             Arg(name='fuga', description='description of fuga.'),
         ]
 
+    def test_arg_of_lines_numpy(self) -> None:
+        arg_lines = [
+            'hoge: str',
+            '      description of hoge following type',
+            'fuga:',
+            '      description of fuga',
+        ]
+        args = Arg.of_lines_numpy(arg_lines)
+        assert args == [
+            Arg(name='hoge', description='description of hoge following type'),
+            Arg(name='fuga', description='description of fuga'),
+        ]
 
-class TestDocstring(unittest.TestCase):
+    def test_arg_of_lines_resttxt(self) -> None:
+        arg_lines = [
+            ':param hoge: description of hoge.',
+            ':param fuga: description of fuga',
+            '             multilined.',
+        ]
+        args = Arg.of_lines_resttxt(arg_lines)
+        assert args == [
+            Arg(name='hoge', description='description of hoge.'),
+            Arg(name='fuga', description='description of fuga multilined.'),
+        ]
 
-    # -------------------------------------
-    # Google
-    # -------------------------------------
+
+class TestDocstringGoogle(unittest.TestCase):
+
     def test_google_one_line_description(self) -> None:
         docstring = """ This is oneline docstring. """
         parser = GoogleParser()
@@ -48,6 +72,73 @@ class TestDocstring(unittest.TestCase):
             args=[
                 Arg(name='foo', description='Description of foo.'),
                 Arg(name='bar', description='Description of bar.'),
+            ],
+        )
+        return
+
+
+class TestDocstringNumpy(unittest.TestCase):
+
+    def test_numpy_one_line_description(self) -> None:
+        docstring = """ This is oneline docstring. """
+        parser = NumpyParser()
+        doc = parser.parse(docstring)
+        assert doc == Doc(
+            description='This is oneline docstring.',
+            args=[],
+        )
+        return
+
+    def test_numpy_simple(self) -> None:
+        docstring = """
+        This is a simple docstring.
+
+        Parameters
+        ----------
+            foo: str
+                 Description of foo.
+            bar: int
+                 Description of bar.
+        """
+        parser = NumpyParser()
+        doc = parser.parse(docstring)
+        assert doc == Doc(
+            description='This is a simple docstring.',
+            args=[
+                Arg(name='foo', description='Description of foo.'),
+                Arg(name='bar', description='Description of bar.'),
+            ],
+        )
+        return
+
+
+class TestDocstringRestructuredText(unittest.TestCase):
+
+    def test_resttext_one_line_description(self) -> None:
+        docstring = """ This is oneline docstring. """
+        parser = NumpyParser()
+        doc = parser.parse(docstring)
+        assert doc == Doc(
+            description='This is oneline docstring.',
+            args=[],
+        )
+        return
+
+    def test_resttext_simple(self) -> None:
+        docstring = """
+        This is a simple docstring.
+
+        :param foo: Description of foo.
+        :param bar: Description of bar
+                    that is multilined.
+        """
+        parser = RestructuredTextParser()
+        doc = parser.parse(docstring)
+        assert doc == Doc(
+            description='This is a simple docstring.',
+            args=[
+                Arg(name='foo', description='Description of foo.'),
+                Arg(name='bar', description='Description of bar that is multilined.'),
             ],
         )
         return
