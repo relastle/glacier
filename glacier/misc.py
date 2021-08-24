b584b38806302b46ae749e2ac35aeeb1bb7dc7ff
Hiroki Konishi <hkonishi@yahoo-corp.jp>
2021-08-24 16:10:19 +0900

    update: linter / dev-dependencies / asyncio entrypoint

--- glacier/misc.py
+++ glacier/misc.py
@@ -7,11 +7,13 @@ from typing import Any
 # https://github.com/pallets/click/issues/85#issuecomment-503464628
 def coro(f: Any) -> Any:
     if not inspect.iscoroutinefunction(f):
-        'not Coroutine'
+        # not Coroutine
         return f
 
     @wraps(f)  # type: ignore
     def wrapper(*args: Any, **kwargs: Any) -> Any:
-        return asyncio.run(f(*args, **kwargs))  # type: ignore
-
+        return (
+            asyncio.get_event_loop()
+            .run_until_complete(f(*args, **kwargs))
+        )  # type: ignore
     return wrapper
