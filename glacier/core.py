55ef08cb315a12ad4107ba27dc38e8924dab50d7
Nick Shook <nick.shook@apple.com>
2022-03-17 21:22:57 -0700

    update click & click-help-colors

--- glacier/core.py
+++ glacier/core.py
@@ -1,15 +1,19 @@
 import functools
 from enum import Enum
 from inspect import Parameter, signature
-from typing import (Any, Callable, Coroutine, Dict, List, Optional, Type,
-                    TypeVar, Union)
+from typing import Any, Callable, Coroutine, Dict, List, Optional, Type, TypeVar, Union
 
 import click
 import click_completion
 from click_help_colors import HelpColorsCommand, HelpColorsGroup
 
-from glacier.docstring import (Doc, GoogleParser, NumpyParser, Parser,
-                               RestructuredTextParser)
+from glacier.docstring import (
+    Doc,
+    GoogleParser,
+    NumpyParser,
+    Parser,
+    RestructuredTextParser,
+)
 from glacier.misc import coro
 
 """
@@ -19,16 +23,16 @@ from glacier.misc import coro
 - [ ] Parse python docstring to display help.
 """
 
-T = TypeVar('T')
+T = TypeVar("T")
 
 
 CONTEXT_SETTINGS = dict(
-    help_option_names=['-h', '--help'],
+    help_option_names=["-h", "--help"],
     max_content_width=120,
 )
 DEFAULT_COLOR_OPTIONS = dict(
-    help_headers_color='white',
-    help_options_color='cyan',
+    help_headers_color="white",
+    help_options_color="cyan",
 )
 
 
@@ -93,10 +97,7 @@ def _get_best_doc(docstring: str, arg_names: List[str]) -> Doc:
         NumpyParser,
         RestructuredTextParser,
     ]
-    docs = [
-        parser_type().parse(docstring=docstring)
-        for parser_type in parser_types
-    ]
+    docs = [parser_type().parse(docstring=docstring) for parser_type in parser_types]
     return max(docs, key=lambda doc: doc.get_matched_arg_count(arg_names))
 
 
@@ -117,10 +118,7 @@ def _get_click_command(
             arg_names=[param.name for param in sig.parameters.values()],
         )
         f.__doc__ = doc.description
-        arg_help_d = {
-            arg.name: arg.description
-            for arg in doc.args
-        }
+        arg_help_d = {arg.name: arg.description for arg in doc.args}
     else:
         arg_help_d = {}
 
@@ -132,7 +130,7 @@ def _get_click_command(
 
     # Decorate the function reversely.
     for param in reversed(list(sig.parameters.values())):
-        if param.name.startswith('_'):
+        if param.name.startswith("_"):
             # Positional argument
             click_f = click.argument(
                 param.name,
@@ -144,17 +142,17 @@ def _get_click_command(
             if param.default == Parameter.empty:
                 common_kwargs = dict(
                     required=True,
-                    help=arg_help_d.get(param.name, ''),
+                    help=arg_help_d.get(param.name, ""),
                 )
             else:
                 common_kwargs = dict(
                     default=param.default,
-                    help=arg_help_d.get(param.name, ''),
+                    help=arg_help_d.get(param.name, ""),
                 )
             if param.annotation == bool:
                 # Boolean flag
                 click_f = click.option(  # type: ignore
-                    '--' + param.name.replace('_', '-'),
+                    "--" + param.name.replace("_", "-"),
                     is_flag=True,
                     type=bool,
                     **common_kwargs,
@@ -162,13 +160,13 @@ def _get_click_command(
             elif param.annotation == str or param.annotation == int:
                 # string or boolean option
                 click_f = click.option(  # type: ignore
-                    '--' + param.name.replace('_', '-'),
+                    "--" + param.name.replace("_", "-"),
                     type=param.annotation,
                     **common_kwargs,
                 )(click_f)
             elif issubclass(param.annotation, Enum):
                 click_f = click.option(  # type: ignore
-                    '--' + param.name.replace('_', '-'),
+                    "--" + param.name.replace("_", "-"),
                     type=click.Choice(enum_map[param.name].keys()),
                     **common_kwargs,
                 )(click_f)
@@ -194,25 +192,28 @@ def rename(
     @functools.wraps(f)
     def wrapped(*args: Any, **kwargs: Any) -> T:
         return f(*args, **kwargs)
+
     wrapped.__name__ = name
     return wrapped
 
 
 @click.option(
-    '-i',
-    '--case-insensitive/--no-case-insensitive',
+    "-i",
+    "--case-insensitive/--no-case-insensitive",
     help="Case insensitive completion",
 )
 @click.argument(
-    'shell',
+    "shell",
     required=False,
     type=click_completion.DocumentedChoice(click_completion.core.shells),
 )
 def show_completion(shell: str, case_insensitive: bool) -> None:
     """Show the click-completion-command completion code"""
-    extra_env = {
-        '_CLICK_COMPLETION_COMMAND_CASE_INSENSITIVE_COMPLETE': 'ON'
-    } if case_insensitive else {}
+    extra_env = (
+        {"_CLICK_COMPLETION_COMMAND_CASE_INSENSITIVE_COMPLETE": "ON"}
+        if case_insensitive
+        else {}
+    )
     click.echo(click_completion.core.get_code(shell, extra_env=extra_env))
 
 
@@ -275,11 +276,13 @@ def glacier_group(
     return group  # type: ignore
 
 
-def glacier(f: Union[
-    GlacierFunction,
-    List[GlacierFunction],
-    Dict[str, Union[GlacierFunction, GlacierUnit]],
-]) -> None:
+def glacier(
+    f: Union[
+        GlacierFunction,
+        List[GlacierFunction],
+        Dict[str, Union[GlacierFunction, GlacierUnit]],
+    ]
+) -> None:
     """
     Main function making function to command line entrypoint
     """
